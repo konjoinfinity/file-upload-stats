5a232032f69dc77d5a282edbf98afde4157d5cf1
Wesley Scholl <clients@konjoweb.com>
2023-03-03 22:38:30 +0300

    all tests are now passing, uploading to gh

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,12 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post("/api/fileanalyse", upload.single('upfile'),(req, res) => {
+  let fileName = req.file.originalname;
+  let fileType = req.file.mimetype;
+  let fileSizeBytes = new Number(req.file.size);
+  res.json({name: fileName, type: fileType, size: fileSizeBytes});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
